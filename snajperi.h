1121bdae0816ed98e0c32fd72b0ee96ff0710a0e
Kraguljac23 <61576481+Kraguljac23@users.noreply.github.com>
2020-05-09 19:49:51 +0200

    Update snajperi.h

--- snajperi.h
+++ snajperi.h
@@ -5,34 +5,22 @@
 
 class sniperi: public oruzje{
 private:
-    double stanje;  //koliko igrac ima trenutno para na svom racunu
+    int ScopeZoom;
 public:
     sniperi():oruzje(){
     tip = SNAJPER;
+    ScopeZoom = 4;
     puc = SINGLE;
     cena = 11000;
     BrMunicije = 15;
     }
 
-    sniperi(int c, int br, double s, int tm, TipOruzja t, TipPucanja p):oruzje(c, br, p, t,tm){
-        stanje = s;
+    sniperi(int c, int br, int sz, int tm, TipOruzja t, TipPucanja p):oruzje(c, br, p, t,tm){
+        ScopeZoom = sz;
     }
 
      sniperi(const sniperi &s):oruzje(s.cena, s.tip, s.puc, s.BrMunicije, s.TrenutnoMunicije){
-        stanje = s.stanje;
-    }
-
-    bool sniperi::buy(){
-    bool uspesno;
-
-    if(stanje >= cena){
-        uspesno = true;
-        stanje -= cena;
-    }else{
-        uspesno = false;
-    cout << "Nemate dovoljno sredstava za kupovinu oruzja." << endl
-    }
-    return uspesno;
+        ScopeZoom = s.ScopeZoom;
     }
 
     bool sniperi::reload(){
@@ -49,8 +37,8 @@ public:
     }
 
 
-    double GetStanje() const{return stanje;}
-    void SetStanje(double stanje)(stanje = s);
+    double GetScopeZoom() const{return ScopeZoom;}
+    void SetScopeZoom(int sz)(ScopeZoom = sz);
 
 };
 
